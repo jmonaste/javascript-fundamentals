27d6f9c20bfd6bc6f6d3b4511db19390533f30d8
javier monasterio <javiermonasterio@gmail.com>
2022-01-20 22:06:53 +0100

    isosceles triangle added

--- figuras.js
+++ figuras.js
@@ -45,6 +45,28 @@ function areaCirculo(radioCirculo) {
 
 console.groupEnd();
 
+//Código del triángulo isósceles
+
+//En geometría, un triángulo isósceles es un tipo de triángulo que tiene, al menos, dos lados de igual longitud. 
+//Al ángulo formado por los lados de igual longitud se le denomina ángulo en el vértice y al lado opuesto a él, 
+//ángulo base.1
+
+//por lo que deberíamos pedir sólo dos datos. Un lado y la base
+
+console.group("Triángulo isósceles");
+
+function alturaIsosceles(ladoIsosceles, baseIsosceles) {
+    /*la altura es la raiz cuadrada de 4 veces el lado al cuadrado - base al cuadrado
+    todo ello dividido entre 2*/
+    return ((Math.sqrt((4*ladoIsosceles*ladoIsosceles)-(baseIsosceles*baseIsosceles))) / (2))
+}
+
+console.groupEnd();
+
+
+
+
+
 
 // utils
 
@@ -118,3 +140,21 @@ function calcularAreaCirculo(radio) {
 }
 
 console.group("Círculo");
+
+
+//Isósceles
+
+console.group("Isósceles");
+
+function calcularAlturaIsosceles() {
+    const inputLadoIsosceles = document.getElementById("inputLadoIsosceles");
+    const inputBaseIsosceles = document.getElementById("inputBaseIsosceles");
+
+    const ladoIsosceles = inputLadoIsosceles.value;
+    const baseIsosceles = inputBaseIsosceles.value;
+
+    const altura = alturaIsosceles(ladoIsosceles, baseIsosceles);
+    alert(altura);
+} 
+
+console.groupEnd();
