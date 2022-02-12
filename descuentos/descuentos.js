6b0a2d483c08a11c03a2d4f41e1c3ad638a4feeb
javier monasterio <javiermonasterio@gmail.com>
2022-02-12 18:52:39 +0100

    Creada la función para calcular el descuento

---
+++ descuentos/descuentos.js
@@ -0,0 +1,25 @@
+//const precioOriginal = 120;
+//const descuento = 18;
+
+
+
+function calcularPrecioConDescuento(precio, descuento) {
+    //calculamos el precio aplicando el descuento sobre el precio pasado por parámetro
+    const porcentajePrecioConDescuento = 100 - descuento;
+    return precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+}
+
+
+
+/*
+console.log("El precio original es " + precioOriginal);
+
+//otra forma de usar el log, enviando un objeto como parámetro de entrada a la función de log
+console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento,
+
+});
+*/
