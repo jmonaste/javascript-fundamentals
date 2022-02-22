1d22e28c6755e5319c11226ccbe55771b8834032
javier monasterio <javiermonasterio@gmail.com>
2022-02-22 18:30:00 +0100

    Se añade nueva versión de la función para el cálculo de la moda

--- media_promedio_mediana/media_promedio_mediana.js
+++ media_promedio_mediana/media_promedio_mediana.js
@@ -206,3 +206,26 @@ function calcularModa(lista) {
     return sumArray.indexOf(max);
 
 }
+
+function calcularModav2(lista) {
+    const listaCount;
+    lista.map(
+        function(elemento) {
+            if(listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            }
+            else {
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+
+    const listaArray = Object.entries(listaCount).sort(
+        function(valorAcumulado, nuevoValor) {
+            return valorAcumulado[1] - nuevoValor[1];
+        }
+    );
+
+    const moda = listaArray[listaArray.length - 1];
+    return moda;
+}
