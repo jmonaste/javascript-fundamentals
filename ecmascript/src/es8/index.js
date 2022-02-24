deb792f4885d55168cee9820fe5a6f9678347921
javier monasterio <javiermonasterio@gmail.com>
2022-02-24 20:28:32 +0100

    ecmascript-8 paddin start and end

--- ecmascript/src/es8/index.js
+++ ecmascript/src/es8/index.js
@@ -20,3 +20,21 @@ console.log(entries.length);
 //object.values - me devuelve los valores de un objeto a un array
 const values = Object.values(data);
 console.log(values);
+
+/**
+ * tambien se añadió el padding
+ */
+
+const string = 'Hello!';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padStart(8, 'h'));
+console.log(string.padEnd(12, 'hii'));
+
+//sirve para poder presentar una estructura de elementos. un menu, estructura, etc, para la opcion de impresion
+//lo podemos hacer de esta forma
+console.log('food'.padEnd(12,'---------'));
+console.log('drinks'.padEnd(12,'---------'));
+console.log('cofees'.padEnd(12,'---------'));
+console.log('bee'.padEnd(12,'---------'));
+console.log('a'.padEnd(12,'---------'));
+console.log('nowhere'.padEnd(12,'---------'));
