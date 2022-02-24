a38949ee4a5408099a83b9a67dc3d8bb2ba49ead
javier monasterio <javiermonasterio@gmail.com>
2022-02-24 20:18:21 +0100

    ecmascript-8 object.entries

---
+++ ecmascript/src/es8/index.js
@@ -0,0 +1,15 @@
+/**
+ * se ha introducido object.entries
+ * que nos va a permitir devolver la clave y valores de una matriz 
+ */
+
+const data = {
+    frontend: 'Javier',
+    backend: 'Isabel',
+    designer: 'Ana'
+};
+
+//como pasamos este objeto a matriz?
+const entries = Object.entries(data);
+console.log(entries);
+
