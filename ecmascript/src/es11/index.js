32a947028c93d8c00c528b2032beac1320210670
javier monasterio <javiermonasterio@gmail.com>
2022-02-26 00:17:34 +0100

    promise.allsettled

--- ecmascript/src/es11/index.js
+++ ecmascript/src/es11/index.js
@@ -34,3 +34,15 @@ const anotherBigNumber = BigInt(9007199254740991);
 
 console.log(aBigNumber);
 console.log(anotherBigNumber);
+
+
+
+//Promise All Settled
+
+const promise1 = new Promise((resolve,reject) => reject("reject"));
+const promise2 = new Promise((resolve,reject) => resolve("resolve"));
+const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
+
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
