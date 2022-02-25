372f45b15ff3248f8b11c6604055a42ebef00bc3
javier monasterio <javiermonasterio@gmail.com>
2022-02-25 23:50:27 +0100

    array.trimstart

--- ecmascript/src/es10/index.js
+++ ecmascript/src/es10/index.js
@@ -23,4 +23,11 @@ console.log(array.flat(2));
  let array = [1,2,3,4,5];
  console.log(array.flatMap(value => [value, value * 2]));
 
- 
+ /**
+  * trimstart()
+  * elimina espacios en blancos de un string
+  */
+
+ let helloWorld = '                hello World';
+ console.log(helloWorld);
+ console.log(helloWorld.trimStart());
