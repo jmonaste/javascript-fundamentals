de6bdddbd18c884d7cb6a99f9faf6aa0195d2a7e
javier monasterio <javiermonasterio@gmail.com>
2022-02-25 23:30:58 +0100

    promise.finally

--- ecmascript/src/es9/index.js
+++ ecmascript/src/es9/index.js
@@ -11,3 +11,56 @@ const obj = {
     age: 30,
     country: 'Spain',
 }
+
+let { name, ...all } = obj;
+console.log(name, all);
+
+
+/**
+ * Más sobre el operador de reposo
+ */
+
+const obj = {
+    name: 'Javier',
+    age: 30,
+}
+
+const obj1 = {
+    ...obj,
+    country: 'Spain'
+}
+
+console.log(obj1);
+
+/**
+ * promise.finally
+ * podmeos saber cuando ha temrinado el llamado y poder ejecutar funcion o logica según el caso
+ */
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? resolve('Hello World')
+        : reject(new Error('Test error'))
+    });
+};
+
+helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizado'))
+
+//añadimos algo de complejidad
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test error'))
+    });
+};
+
+helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizado'))
