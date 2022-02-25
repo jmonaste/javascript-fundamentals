86244474155970b264c758cea1459c717abdea30
javier monasterio <javiermonasterio@gmail.com>
2022-02-26 00:18:40 +0100

    globalthis

--- ecmascript/src/es11/index.js
+++ ecmascript/src/es11/index.js
@@ -46,3 +46,9 @@ const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
 
 Promise.allSettled([promise1, promise2, promise3])
     .then(response => console.log(response));
+
+
+//Global This
+
+console.log(window);
+console.log(globalThis);
