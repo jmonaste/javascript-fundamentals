4114b6128d78362959d42ea5b6169302f6742773
javier monasterio <javiermonasterio@gmail.com>
2022-02-25 15:09:28 +0100

    ecmascript-9 operador de reposo

---
+++ ecmascript/src/es9/index.js
@@ -0,0 +1,13 @@
+/**
+ * caracteristicas de ecmascript9
+ */
+
+
+/**
+ * operador de reposo. puede extraer las prop deun objeto que aun no se ha construido
+ */
+const obj = {
+    name: 'Javier',
+    age: 30,
+    country: 'Spain',
+}
