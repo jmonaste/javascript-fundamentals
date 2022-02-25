281b799408a3d81d17555202b8f56e4884dd8696
javier monasterio <javiermonasterio@gmail.com>
2022-02-25 23:37:03 +0100

    regex match

--- ecmascript/src/es9/index.js
+++ ecmascript/src/es9/index.js
@@ -64,3 +64,22 @@ helloWorld()
     .then(response => console.log(response))
     .catch(error => console.log(error))
     .finally(() => console.log('Finalizado'))
+
+
+/**
+ * han añadido mejoras sobre regex
+ * cómo podemos agrupar bloques del regex y pdoder acceder a cada uno de ellos
+ */
+
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = regexData.exec('2018-04-20');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
+
+
+
+
+
