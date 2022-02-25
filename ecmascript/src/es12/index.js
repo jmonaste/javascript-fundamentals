426d5358238bfe4ac7c72c9c318a14b57b50b713
javier monasterio <javiermonasterio@gmail.com>
2022-02-26 00:32:46 +0100

    promise.any

--- ecmascript/src/es12/index.js
+++ ecmascript/src/es12/index.js
@@ -29,3 +29,13 @@ class Message {
 
 const message = new Message();
 message.show('Hola!');
+
+
+//Promise Any
+
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
