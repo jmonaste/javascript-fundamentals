a961410fb7f36de6f557a99a03ca34d8be48cf51
javier monasterio <javiermonasterio@gmail.com>
2022-02-28 23:46:57 +0100

    continuando con asincronismo en javascript

--- asincronismo/src/callback/index.js
+++ asincronismo/src/callback/index.js
@@ -1,3 +1,15 @@
+/**
+ * Callbacks
+ */
+
+
+
+/**
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns 
+ */
 function sum(num1, num2) {
     return num1 + num2;
 }
@@ -15,3 +27,22 @@ function calc(num1, num2, callback) {
 
 //creamos el llamado
 console.log(calc(2,2,sum));
+
+
+//hemos creado una funcion que recibe como argumento el callback
+
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(function () {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate(dateNow) {
+    console.log(dateNow);
+}
+
+
+date(printDate);
