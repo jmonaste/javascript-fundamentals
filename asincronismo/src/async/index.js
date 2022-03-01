131a0b92f8107bbdf450c08e21d94af8d239c438
javier monasterio <javiermonasterio@gmail.com>
2022-03-01 18:39:37 +0100

    trabajando con async

---
+++ asincronismo/src/async/index.js
@@ -0,0 +1,24 @@
+/**
+ * antes vimos los callbacks y las promesas y resolvimos el reto
+ * ahora tendmeos que ver async y await
+ * este nuevo enfoque que nace dentro de ecmascript, garantiza que nosotros
+ * vamos a implementar nuestro codigo. es preferible a las promesas porque
+ * hace que nuestro código se ejecute de forma síncrona
+ */
+
+const doSomethingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Do Something Async'), 3000)
+        : reject(new Error('Test Error'))
+    })
+}
+
+const doSomething = async () => {
+    const something = await doSomethingAsync()
+    console.log(something);
+}
+
+console.log('Before');
+doSomething();
+console.log('After');
